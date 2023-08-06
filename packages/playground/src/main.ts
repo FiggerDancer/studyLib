c92379c487005993fd9a9cdc73be11ae9d2e9168
FlyCat <ysulixiangyuan@gmail.com>
2023-08-06 17:36:03 +0800

    feat: 新增动态规划

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -3,60 +3,43 @@ import App from './App.vue'
 import { router } from './router'
 import '@/styles/main.css'
 
-
-
-function add(a: number, b: number) {
-    let sum = a
-    while (b !== 0) {
-        sum = a ^ b
-        b = (a & b) << 1
-        a = sum
-    }
-    return sum
-}
-
-function negNum(n: number) {
-    return add(~n, 1)
-}
-
-function minus(a: number, b: number) {
-    return add(a, negNum(b))
-}
-
-function multi(a: number, b: number) {
-    let res = 0
-    while (b !== 0) {
-        // b的最后一位是否为0，是0则不加，是1则加上
-        if ((b & 1) !== 0) {
-            res = add(res, a)
-        }
-        // a向左移
-        a  = a << 1
-        // b向右移,无符号
-        b >>>= 1
-    }
-    return res
-}
-
-function isNeg(num: number) {
-    return num < 0
-}
-
-function div(a: number, b: number) {
-    let x = isNeg(a) ? negNum(a) : a
-    const y = isNeg(b) ? negNum(b) : b
-    let res = 0
-    for (let i = 31; i > -1; i = minus(i, 1)) {
-        if ((x >> i) >= y) {
-            res |= (1 << i)
-            x = minus(x, y << i)
-        }
-    }
-    return (isNeg(a) === isNeg(b)) ? res : negNum(res) 
-}
-
-console.log(div(12,4))
-
 createApp(App)
     .use(router)
     .mount('#app');
+
+function getWays1(arr: number[], aim: number) {
+        const n = arr.length
+        const dp = new Array(n + 1).fill(0).map(() => new Array(aim + 1).fill(0))
+        dp[n][0] = 1
+        for (let index = n - 1; index >= 0; index--) {
+            for (let rest = 0; rest <= aim; rest++) {
+                let ways = 0
+                for (let zhang = 0; arr[index] * zhang <= rest; zhang++) {
+                    ways += dp[index + 1][rest - arr[index] * zhang]
+                }
+                dp[index][rest] = ways
+            }
+        }
+        return dp[0][aim]
+    }
+
+
+function getWays(arr: number[], aim: number) {
+    const n = arr.length
+    const dp = new Array(n + 1).fill(0).map(() => new Array(aim + 1).fill(0))
+    dp[n][0] = 1
+    for (let index = n - 1; index >= 0; index--) {
+        for (let rest = 0; rest <= aim; rest++) {
+            // 这一行前面的值，也是通过下面的值进行计算的也就说，这些东西已经算过一遍了
+            // 这里可以直接通过该值下面的值和左边的上个值相加取得
+            dp[index][rest] = dp[index + 1][rest]
+            if (rest - arr[index] >= 0) {
+                dp[index][rest] += dp[index][rest - arr[index]]
+            }
+        }
+    }
+    console.log(dp)
+    return dp[0][aim]
+}
+
+console.log(getWays1([1,5,3,2], 10), getWays([1,5,3,2], 10))
